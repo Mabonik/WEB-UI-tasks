876651292a6c0c1eed17a3402ed14c3de28c12ae
Nastya <netkachova.nastya@gmail.com>
2021-07-15 15:07:59 +0300

    modified 2 tasks

--- task1.js
+++ task1.js
@@ -1,17 +1,21 @@
 //Task #178(г)
 
-const array = [1,8,3,4,5];
-const result = [];
-for(let i = 0; i < array.length; i++){
-
-  const k = array[i];
-  const previous = array[i - 1] || 0;
-  const next = array[i + 1] || 0;
-  if(k < (previous + next) / 2){
-      result.push(k)
+function solution(array){
+  const result = [];
+  for(let i = 0; i < array.length; i++){
+    const k = array[i];
+    const previous = array[i - 1] || 0;
+    const next = array[i + 1] || 0;
+    if(k < (previous + next) / 2){
+        result.push(k)
+    }
   }
+  return result
 }
 
-console.log(result);
+console.log(solution([1,8,3,4,5]));//expecteed result [1,3]
+console.log(solution([1,13,3,87,5]));//expecteed result [1,3,5]
+console.log(solution([6,8,3,7,53]));//expecteed result [3,7]
+
 
 
--- task2.js
+++ task2.js
@@ -1,24 +1,26 @@
 //Task #178(д)
 
-const result2= [];
-
-for(let i = 0; i < array.length; i++){
-
-  const k = array[i];
-  const inPow = Math.pow(2, i);
-  const inFactorial = factorial(i)
-
-  if(inPow < k < inFactorial){
-    result2.push(k)
+function solution(array) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    const k = array[i];
+    const inPow = Math.pow(2, i);
+    const inFactorial = factorial(i);
+    if (inPow < k < inFactorial) {
+      result.push(k);
+    }
   }
-}
+  return result;
 
-function factorial(n){
-  if(n === 0 || n === 1){
+  function factorial(n) {
+    if (n === 0 || n === 1) {
       return 1;
-  }else{
-      return n * factorial(n-1);
+    } else {
+      return n * factorial(n - 1);
+    }
   }
 }
 
-console.log(result2);
+console.log(solution([1, 8, 3, 4, 5])); //expected result [1, 3, 4, 5]
+console.log(solution([11, 8, 3, 44, 5])); //expected result [3, 44, 5]
+console.log(solution([1, 88, 31, 4, 5])); //expected result [1, 31, 4, 5]
